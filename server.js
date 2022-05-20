3711114106443b9dd368bdd418a88bd296ffa352
Takoyarou <kou1105dane@gmail.com>
2022-05-20 14:52:00 +0900

    release?

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var port = 3000;
 const path = require('path');
 express = require('express');
-app = express().use(express.static(path.resolve(__dirname, './')));
+app = express().use(express.static(path.resolve(__dirname, 'app')));
 http = require('http').Server(app);
 app.get('/', function(req, res){
-    res.sendFile(path.resolve(__dirname, './app/index.html'));
+    res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 app.get('/collection', function(req, res){
-    res.sendFile(path.resolve(__dirname, './app/index.html'));
+    res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 http.listen(port, function(){
     console.log("Node server listening on port " + port);
