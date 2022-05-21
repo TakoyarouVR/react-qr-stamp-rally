2d791e36f522b3cc2362b8e272dda300ad7d45a3
TakoyarouVR <kou1105dane@gmail.com>
2022-05-21 09:14:27 +0100

    release

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ var port = 3000;
 fs = require('fs');
 const path = require('path');
 express = require('express');
-http = require('http');
 https = require('https');
 var server = express();
-http.createServer(server).listen(3001)
+server.use(express.static('./app'));
 server.get('/', function(req, res){
     res.sendFile(path.resolve(__dirname, 'app/index.html'));
 });
@@ -13,8 +12,8 @@ server.get('/collection', function(req, res){
     res.sendFile(path.resolve(__dirname, 'app/index.html'));
 });
 var sslOptions = {
-    key: fs.readFileAsync('key.pem'),
-    cert: fs.readFileAsync('cert.pem')
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readFileSync('./cert.pem')
 }
 
-https.createServer(sslOptions, server).listen(3000)
+https.createServer(sslOptions, server).listen(port)
