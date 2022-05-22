1d6540b9ef41a02df18c8f96cc630294b084ba00
Takoyarou <kou1105dane@gmail.com>
2022-05-22 12:18:38 +0900

    a

--- src/ShowImage.js
+++ src/ShowImage.js
@@ -45,7 +45,7 @@ function Show({pic, n}){
         marginBottom: "-3.5%",
         marginLeft: "10%",
         marginRight: "10%",
-        maxWidth: "30vw"
+        maxWidth: "25vw"
     }    
     if(c == n)
     {
