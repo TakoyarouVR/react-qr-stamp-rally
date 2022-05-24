64421694d432f0d689bedbae3eb8322273dd4bbe
Takoyarou <kou1105dane@gmail.com>
2022-05-24 19:39:33 +0900

    scan on load

--- src/CameraPage.js
+++ src/CameraPage.js
@@ -1,43 +1,34 @@
-import React, { Component} from 'react';
+import React from 'react';
 import { QrReader } from 'react-qr-reader';
 import StyledButton from "./StyledButton";
 import Music from './audios/Get.mp3';
+import {useNavigate} from 'react-router-dom';
 
 const buttonProp = {
     txt: "スタンプを見る",
     refer: "/collection",
 }
-class CameraPage extends Component {
-    se = new Audio(Music);
-    constructor(props){
-        super(props);
-        this.handleScan = this.handleScan.bind(this);
-    }
-    //localStorageに変更
-    handleSetCookie = (data) => {
-        localStorage.setItem(data, data);
-    };
-
-    handleScan = (data) => {
+const CameraPage = () => {
+    const navigate = useNavigate();
+    const se = new Audio(Music);
+    const handleScan = (data) => {
         if(data != null){
-            this.se.play();
-            this.handleSetCookie(data.text);
+            se.play();
+            localStorage.setItem(data.text, data.text);
+            navigate('/collection');
         }
-    }
-
-    render() {                     
-        return (
-            <div>
-                <StyledButton data={buttonProp}/>
-                <QrReader
-                    constraints={{facingMode: "environment"}}
-                    delay={100}
-                    onResult={this.handleScan}
-                    videoStyle={{zIndex: 0,width: "100%", marginTop: "30%"}}
-                    videoContainerStyle={{zIndex: 0, position: "fixed", top: 0,left: 0}}
-                /> 
-            </div>
-        );
-    }
+    }                  
+    return (
+        <div>
+            <StyledButton data={buttonProp}/>
+            <QrReader
+                constraints={{facingMode: "environment"}}
+                delay={100}
+                onResult={handleScan}
+                videoStyle={{zIndex: 0,width: "100%", marginTop: "30%"}}
+                videoContainerStyle={{zIndex: 0, position: "fixed", top: 0,left: 0}}
+            /> 
+        </div>
+    );
 }
 export default CameraPage;
